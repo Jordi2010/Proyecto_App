1508504e04330fe4a041925dc52836ddb5549351
josers77j <josers77.1@gmail.com>
2024-03-17 15:29:39 -0600

    Agregar archivos de proyecto.

---
+++ zionAppSensorial/zionAppSensorial/controllers/exampleModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zionAppSensorial.controllers
+{
+    public class exampleModel
+    {
+    }
+}
---
+++ zionAppSensorial/zionAppSensorial/views/exampleView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zionAppSensorial.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class exampleView : ContentPage
+    {
+        public exampleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
